b04c71f4db6b677dab6aa8b6e8735cc536481396
root <root@vwinfra.linux>
2017-06-14 16:47:54 +0200

    Added slope data

--- lanedetect/lanecurvedetect.py
+++ lanedetect/lanecurvedetect.py
@@ -4,8 +4,9 @@ import math
 import matplotlib.pyplot as plt
 from time import sleep
 import csv
+import random
 
-hlines = True
+hlines = False
 
 def main(img):
     
@@ -50,38 +51,12 @@ def main(img):
             p2_y = round(p1_y + length * math.sin(cangle * math.pi / 180))
 
             cv2.line(original, (int(p1_x), int(p1_y)), (int(p2_x), int(p2_y)), (0, 0, 255), 3)
-            cv2.circle(original, (p1_x, p1_y), 4, (0,10,0), 4)
             cv2.circle(original, (p2_x, p2_y), 4, (255,0,), 4)
-
-
-##        angles = [[25], [75], [105], [155]] 
-##
-##        max_width = img.shape[1]
-##        max_height = img.shape[0] - 60
-##        bottom_width = 580
-##
-##        angle = 25
-##        length = 1280
-##        p1_x = (max_width / 2) - bottom_width
-##        p1_y = max_height
-##
-##        theta = angle * math.pi / 180
-##
-##        p2_x = round(p1_x + length * math.cos(angle * math.pi / 180))
-##        p2_y = round(p2_y + length * math.cos(angle * math.pi / 180))
-##
-##        cv2.line(original, (line[0], line[1]), (line[2], line[3]), (0, 0, 0), 3)
-
-
     # 
     img = getRegion(img, vertex)
 
-    #cv2.imshow("Regio", img)
-
     # convert bgr2gray
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
-    #cv2.imshow("cvtColor", img)
 
     # blur
     size = 7
@@ -93,13 +68,9 @@ def main(img):
 
     img = cv2.Canny(img, low_thres, high_thres)
 
-    #cv2.imshow("IMG", img)
-
     #merge original with lines
     edges = np.dstack((img, img, img))
     test = cv2.addWeighted(edges, 2, original, 1, 0)
-
-    #cv2.imshow("Image", test)
 
     drawLines(img, original)
 
@@ -203,25 +174,33 @@ def evaluateLine(line, img, original):
         
         for x1, y1, x2, y2 in line:
 
+            old_x = (x1 + x2) / 2
+            old_y = (y1 + y2) / 2
+
+            #print("x = " + str(old_x))
+            #print("y = " + str(old_y))
+
+        
+            #print(line)
+
             y1 = -(y1 - height) 
             y2 = -(y2 - height) 
-
-            #print(str(x1) + " => " + str(x2))
-            #print(str(y1) + " => " + str(y2))
             
             x = (x1 + x2) / 2
             y = (y1 + y2) / 2
 
-            center = (x, y)
+            #print(str(old_x) + " = > " + str(old_y))
+
+            #cv2.circle(original, (int(x), int(y)), 4, (255,0,0), 4)
+
+            center = (int(old_x), int(old_y))
             angle = math.atan2((y2 - y1), (x2 - x1)) * 180 / math.pi
-
-
 
             if math.fabs(angle) <= 10:
                 continue
             
             if x == 0:
-                qqdx = 1
+                x = 1
 
             dx = (x2 - x1)
             dy = (y2 - y1)
@@ -235,24 +214,25 @@ def evaluateLine(line, img, original):
             if angle < 0.0:
                 angle += 180
 
-            #print(angle)
-
-            #current_slope = (y2 - y1) / (x2 - x1)
-            #intercept = y1 - current_slope * x1
-
-            #x_min = x1
-            #y_min = y1
-            #x_max = x2
-            #y_max = y2
-
-            #print("angle = " + str(angle))
+            current_slope = (y2 - y1) / (x2 - x1)
+            intercept = y1 - current_slope * x1
     
-            lines_list.append((line, angle, center, x, b))
+            lines_list.append([line, angle, center, current_slope, intercept])
             
-    except TypeError:
-        print("Type Error")
+    except TypeError as e:
+        print("Type Error = " + str(e))
 
     return lines_list
+
+def getSlope(x1, y1, x2, y2):
+
+    slope = (y2 - y1) / (x2 - x1)
+    intercept = y1 - current_slope * x1
+
+    return slope, intercept
+
+def extrapolateSlope()
+    
 
 def drawLines(img, original):
 
@@ -263,93 +243,75 @@ def drawLines(img, original):
     color = (0, 0, 255)
 
     # sortlines
-
     leftLines = []
     rightLines = []
 
-    for line in lines:
-
-        gradient, intercept = np.polyfit((line[0][0], line[0][1]), (line[0][2], line[0][3]), 1)
-
-        print(gradient)
+    leftSlope = []
+    rightSlope = []
 
     for line in lines:
 
-        #evaluatedLine = evaluateLine(line, img, original)
-        
         for eLItem in evaluateLine(line, img, original):
             
             (x, y) = eLItem[2]
             angle = eLItem[1]
-
-            #print("angle = " + str(angle))
-
+            
             # eliminate lines
             if x < img.shape[1] * 0.5 and angle >= 25 and angle <= 55:
-                leftLines.append(eLItem)
-            #else:
-            #    print(angle)
 
+                slope = eLItem[3]
+                intercept = eLItem[4]
+
+                leftSlope.append((slope, intercept))
+                leftLines.append(eLItem)
             
-            if x > img.shape[1] * 0.5and angle >= 125 and angle <= 155:
+            if x > img.shape[1] * 0.5 and angle >= 125 and angle <= 155:
+                
+                slope = eLItem[3]
+                intercept = eLItem[4]
+
+                rightSlope.append((slope, intercept))
                 rightLines.append(eLItem)
-            #else:
-                #print(angle)
+
+    print(leftSlope)
+    print(rightSlope)
 
     for ll in leftLines:
         line = ll[0][0]
         center = ll[2]
+        slope = ll[3]
+        intercept = ll[4]
+
+        leftSlope.append((slope, intercept))
+
+        #print(center)
+
+        b = random.randint(0,255)
+        g = random.randint(0,255)
+        r = random.randint(0,255)
         
-        cv2.line(original, (line[0], line[1]), (line[2], line[3]), (255, 255, 255), 1)
+        cv2.line(original, (line[0], line[1]), (line[2], line[3]), (b, g, r), 2)
+        cv2.circle(original, (center), 4, (b,g,r), 4)
 
 
     for rl in rightLines:
         line = rl[0][0]
-        center = ll[2]
+        center = rl[2]
+        slope = ll[3]
+        intercept = ll[4]
         
-        cv2.line(original, (line[0], line[1]), (line[2], line[3]), (255, 255, 255), 1)
+        #print(center)
 
+        b = random.randint(0,255)
+        g = random.randint(0,255)
+        r = random.randint(0,255)
+        
+        cv2.line(original, (line[0], line[1]), (line[2], line[3]), (b, g, r), 2)
+        cv2.circle(original, (center), 4, (b,g,r), 4)
 
-##    processLine(rightLines, original, True)
-##    processLine(leftLines, original, False)
-
-      
-##    for ll in leftLines:
-##        for rl in rightLines:
-##
-##            ll_x, ll_y = ll[2]            
-##            cv2.circle(original, (math.ceil(ll_x), math.ceil(ll_y)), 4, (255,0,0), 4)
-##
-##            rl_x, rl_y = rl[2]
-##            cv2.circle(original, (math.ceil(rl_x), math.ceil(rl_y)), 4, (0,0,255), 2)
-
-
-def processLine(lane, img, right):
-
-    #print(right)
-
-    leftLine = []
-    rightLine = []
-
-    for i in range(0, len(lane)):
-
-        if right:
-            rightLine.append(lane[i][2])
-            
-        else:
-            leftLine.append(lane[i][2])
-
-    
-    if right == True:
-        rightLine = sorted(rightLine, key=lambda x: x[1])
-        #cv2.line(img, (int(rightLine[0][0]), int(rightLine[0][1])), (int(rightLine[len(rightLine)-1][0]), int(rightLine[len(rightLine)-1][1])), (0, 0, 0), 3)
-    else:
-        leftLine = sorted(leftLine, key=lambda x: x[1])
-        #cv2.line(img, (int(leftLine[0][0]), int(leftLine[0][1])), (int(leftLine[len(leftLine)-1][0]), int(leftLine[len(leftLine)-1][1])), (0, 0, 0), 3)
- 
 if __name__ == "__main__":
 
-    content_type = "video"
+    content_type = "image"
 
     if content_type == "image":
         img = cv2.imread("image/test1.jpg", 1)
@@ -380,6 +342,3 @@ if __name__ == "__main__":
 
         capture.release()
         cv2.destroyAllWindows()
-        
-
-    
